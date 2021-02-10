e59691b7a020c8359022625e318c8f4f0baa2701
Lewis Sneddon <>
2021-02-10 10:05:25 +0000

    boolean flags for closed and verified

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -2,16 +2,32 @@ import java.math.BigDecimal;
 
 public class Account {
     private boolean isVerified;
+    private boolean isClosed;
+    private BigDecimal balance;
+
+    public Account() {
+        this.balance = BigDecimal.ZERO;
+    }
 
     public void holderVerified() {
         this.isVerified = true;
     }
 
-    public void deposit(BigDecimal amount) {
+    public void closeAccount() {
+        this.isClosed = true;
+    }
 
+    public void deposit(BigDecimal amount) {
+        if (this.isClosed)
+            return;
+        this.balance = this.balance.add(amount);
     }
 
     public void withdraw(BigDecimal amount) {
-
+        if (this.isVerified)
+            return;
+        if (this.isClosed)
+            return;
+        this.balance = this.balance.subtract(amount);
     }
 }
