9bc852530c03d1224a5ec44558ac5c12de59d6d9
Lewis Sneddon <>
2021-02-10 10:00:10 +0000

    Initial commit

---
+++ src/main/java/Account.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+
+public class Account {
+    private boolean isVerified;
+
+    public void holderVerified() {
+        this.isVerified = true;
+    }
+
+    public void deposit(BigDecimal amount) {
+
+    }
+
+    public void withdraw(BigDecimal amount) {
+
+    }
+}
